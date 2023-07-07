c55ac314705269d4792de4f2b58ec629c6f28ae3
eaintm <EAINTPHOOMAY@GMAIL.COM>
2023-07-07 06:21:58 -0400

    add footer component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,13 @@
+import Footer from "./components/Footer";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head />
-      <body>{children}</body>
+      <body>{children}
+      {/*Footer*/}
+      <Footer />
+      </body>
     </html>
   )
 }
