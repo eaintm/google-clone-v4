36b01e8a442902785f2fdfc38b3f77447598008b
eaintm <EAINTPHOOMAY@GMAIL.COM>
2023-07-17 00:58:36 -0400

    add pagination component

--- src/app/components/WebSearchResults.jsx
+++ src/app/components/WebSearchResults.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Link from 'next/link';
 import  Parser  from "html-react-parser";
+import PaginationButtons from './PaginationButtons';
 
 
 export default function WebSearchResults({results}) {
   return (
-    <div className='w-full mx-auto px-3 pb-24 sm:pl-[5%] md:pl-[14%] lg:pl'>
+    <div className='w-full mx-auto px-3 pb-40 sm:pb-24 sm:pl-[5%] md:pl-[14%] lg:pl'>
         <p>
 
     About {results.searchInformation?.formattedTotalResults} results (
@@ -26,6 +27,7 @@ export default function WebSearchResults({results}) {
         </div>
      </div>
     ))}
+        <PaginationButtons/>
     </div> 
   );
 }
